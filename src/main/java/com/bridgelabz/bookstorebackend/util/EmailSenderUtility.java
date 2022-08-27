45edde352a93b915490a5c1e84a45bb6acd5063e
nk400 <navkr007@gmail.com>
2022-08-27 18:24:18 +0530

    ADDED: Created all Layers and added the Utility services and configuration files

---
+++ src/main/java/com/bridgelabz/bookstorebackend/util/EmailSenderUtility.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.bookstorebackend.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailSenderUtility {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail, String subject, String body ) {
+        SimpleMailMessage message=new SimpleMailMessage();
+        message.setFrom("navkr007@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        mailSender.send(message);
+        System.out.println("Mail sent to the User...!");
+
+    }
+}
